a9ab39074a81f8751b95915f151b36f27ef53d6b
Joshua Tang <joshua.y.tang@gmail.com>
2017-01-24 16:55:23 -0800

    reorganization, basic makefile added

---
+++ tests/src/press.asm
@@ -0,0 +1,182 @@
+#target tap
+
+; sync bytes:
+headerflag:     equ 0
+dataflag:       equ 0xff
+
+; some Basic tokens:
+tCLEAR		equ     $FD             ; token CLEAR
+tLOAD		equ     $EF             ; token LOAD
+tCODE		equ     $AF             ; token CODE
+tPRINT		equ     $F5             ; token PRINT
+tRANDOMIZE	equ     $F9             ; token RANDOMIZE
+tUSR		equ     $C0             ; token USR
+
+
+pixels_start	equ	0x4000		; ZXSP screen pixels
+attr_start	equ	0x5800		; ZXSP screen attributes
+printer_buffer	equ	0x5B00		; ZXSP printer buffer
+code_start	equ	24000
+
+; ---------------------------------------------------
+;		a Basic Loader:
+; ---------------------------------------------------
+
+#code PROG_HEADER,0,17,headerflag
+	defb    0
+	defb    "mloader   "
+	defw    variables_end-0
+	defw    10
+	defw    program_end-0
+
+#code PROG_DATA,0,*,dataflag
+
+; 10 CLEAR 23999
+        defb    0,10                    ; line number
+        defb    end10-($+1)             ; line length
+        defb    0                       ; statement number
+        defb    tCLEAR                  ; token CLEAR
+        defm    "23999",$0e0000bf5d00   ; number 23999, ascii & internal format
+end10:  defb    $0d                     ; line end marker
+
+; 20 LOAD "" CODE 24000
+        defb    0,20                    ; line number
+        defb    end20-($+1)             ; line length
+        defb    0                       ; statement number
+        defb    tLOAD,'"','"',tCODE     ; token LOAD, 2 quotes, token CODE
+        defm    "24000",$0e0000c05d00   ; number 24000, ascii & internal format
+end20:  defb    $0d                     ; line end marker
+
+; 30 RANDOMIZE USR 24000
+        defb    0,30                    ; line number
+        defb    end30-($+1)             ; line length
+        defb    0                       ; statement number
+        defb    tRANDOMIZE,tUSR         ; token RANDOMIZE, token USR
+        defm    "24000",$0e0000c05d00   ; number 24000, ascii & internal format
+end30:  defb    $0d                     ; line end marker
+
+program_end:
+
+variables_end:
+
+; ---------------------------------------------------
+;		a machine code block:
+; ---------------------------------------------------
+
+#code CODE_HEADER,0,17,headerflag
+	defb    3			; Indicates binary data
+	defb    "mcode     "	  	; the block name, 10 bytes long
+	defw    code_end-code_start	; length of data block which follows
+	defw    code_start		; default location for the data
+	defw    0       		; unused
+
+
+#code CODE_DATA, code_start,*,dataflag
+
+
+; This code moves a character up, left, down, or right based on the HJKL keys
+
+; Z80 assembler code and data
+	org 32687
+
+	ld a,49         ; blue ink (1) on yellow paper (6*8).
+	ld (23693),a    ; set our screen colours.
+	call 3503       ; clear the screen.
+
+	ld hl,udgs      ; UDGs.
+	ld (23675),hl   ; set up UDG system variable.
+	ld a,2          ; 2 = upper screen.
+	call 5633       ; open channel.
+
+
+loop:
+
+    call read_kbd   ; update our position based on input
+	call setxy      ; set up our x/y coords.
+	ld a,144        ; show UDG instead of asterisk.
+	rst 16          ; display it.
+	call delay      ; want a delay.
+	call setxy      ; set up our x/y coords.
+	ld a,32         ; ASCII code for space.
+	rst 16		; delete old asterisk.
+	call setxy      ; set up our x/y coords.
+    jp loop
+delay:
+	ld b,2          ; length of delay.
+delay0:
+	halt            ; wait for an interrupt.
+	djnz delay0     ; loop.
+	ret             ; return.
+
+setxy:
+	ld a,22         ; ASCII control code for AT.
+	rst 16          ; print it.
+	ld a,(xcoord)   ; vertical position.
+	rst 16          ; print it.
+	ld a,(ycoord)   ; y coordinate.
+	rst 16          ; print it.
+	ret
+
+read_kbd:
+    ld bc,$BFFE     ; load keys HJKL(enter)
+    in d,(c)        ; get status of keys
+    ld a,d
+    and $02         ; check if L is pressed
+    jr z, move_right
+    ld a,d
+    and $04
+    jr z, move_up
+    ld a,d
+    and $08
+    jr z, move_down
+    ld a,d
+    and $10
+    jr z, move_left
+    ret
+
+; move character in a direction based on keypress
+move_up:
+    ld hl, xcoord
+    ld a,(hl)      ; are we at top of screen
+    cp 0
+    ret z
+    dec (hl)
+    ret
+
+move_down:
+    ld hl, xcoord
+    ld a,(hl)      ; are we at bottom of screen
+    cp 21
+    ret z
+    inc (hl)
+    ret
+
+move_left:
+    ld hl, ycoord
+    ld a,(hl)      ; are we at left edge of screen
+    cp 0
+    ret z
+    dec (hl)
+    ret
+
+move_right:
+    ld hl, ycoord
+    ld a,(hl)      ; are we at right edge of screen
+    cp 31
+    ret z
+    inc (hl)
+    ret
+
+xcoord:
+	defb 0
+ycoord:
+	defb 0
+color_flag:
+	defb 0
+udgs:
+	; pacman thing
+	defb 60,126,219,153
+	defb 255,255,219,219
+
+
+code_end:
