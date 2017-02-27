0c2401c0250e6db1be5a1af29ad4010d4bb06f50
Kenta Kitada <kentakitada@gmail.com>
2017-02-27 01:17:30 -0800

    Added a settle puyo routine

--- src/game_utils.asm
+++ src/game_utils.asm
@@ -1,5 +1,5 @@
 ; ------------------------------------------------------------
-; rand8- gets a random value based on the R register
+; rand8: gets a random value based on the R register
 ; ------------------------------------------------------------
 ; Input: None
 ; Output: a - a random 8-bit number
@@ -21,7 +21,7 @@ rand8:
     ret
 
 ; ------------------------------------------------------------
-; spawn_puyos - spawns two randomly colored puyos
+; spawn_puyos: spawns two randomly colored puyos
 ; ------------------------------------------------------------
 ; Input: None
 ; Output: next_puyos: two randomly colored puyos
@@ -39,27 +39,74 @@ next_puyos:
     defb 0, 0
 
 ; ------------------------------------------------------------
-; detect_gameover - checks for a gameover
+; detect_gameover: checks for a gameover
 ; ------------------------------------------------------------
 ; Input: None
 ; Output: a - 0x01 if gameover, otherwise 0x00
 ; ------------------------------------------------------------
 detect_gameover:
-    ld a, (player_board+57)
+    ld a, (player_board+23)
     and 0x03                    ; Isolate status bits
     cp 0x01                     ; Check if puyo exists
-    jp z, game_over
+    jp z, gameover
     ret
 
 ; ------------------------------------------------------------
-; game_over: - the gameover sequence
+; gameover: the gameover sequence
 ; ------------------------------------------------------------
 ; Input: None
 ; Output: None
 ; ------------------------------------------------------------
-game_over:
+gameover:
     ret
 
+; ------------------------------------------------------------
+; settle_puyos: Drops any floating puyos to the ground
+; ------------------------------------------------------------
+; Input: None
+; Output:  a - 1 if this can still be updated, 0 if no more updates
+;          player_board: Updated board
+; ------------------------------------------------------------
+; Note: Board representation is top->down left->right
+; e.g.: 0 11 22 33 44 55
+;       1 12 23 34 45 56
+;       2 13 24 35 46 57
+; This routine first looks for an empty space and moves
+; anything above it downwards.
+; ------------------------------------------------------------
+settle_puyos:
+    ld hl, player_board
+    ld bc, 65                       ; start at last puyo
+    add hl, bc
+    ld d, 0
 
+    ld b, 6                         ; check 6 columns
+settle_puyos_column_loop:
+    ld c, 10                        ; check 10 rows (all but last)
 
+settle_puyos_row_loop:
+    ld a, (hl)                      ; grab a puyo
+    dec hl                          ; point to next
+    cp 0x01                         ; check if empty
+    jp z, settle_puyos_loop_end     ; if not empty, go to next
 
+    ld a, (hl)                      ; grab puyo above
+    cp 0x01
+    jp nz, settle_puyos_loop_end    ; if empty, go to next puyo
+
+    ld (hl), 0                      ; clear above
+    inc hl                          ; point to below
+    ld (hl), a                      ; update with puyo
+    dec hl                          ; return to original position
+
+    ld d, 1                         ; a drop has been made
+
+settle_puyos_loop_end:
+    dec c                           ; repeat this 10 times
+    jp nz, settle_puyos_row_loop
+    dec hl                          ; skip the top row
+    dec b
+    jp nz, settle_puyos_column_loop
+
+    ld a, d                         ; has the board been updated?
+    ret
