9fd8c200e83e7c2a7e3b5b3e509325f837197943
Joshua Tang <joshua.y.tang@gmail.com>
2017-03-11 18:52:18 -0800

    clear sound pitches up instead of down

--- src/sound.asm
+++ src/sound.asm
@@ -85,13 +85,13 @@ puyoclear_se:
 	di
 
 	; initial pitches
-	ld c,70
-	ld e,50
+	ld c,120
+	ld e,100
 	ld b,c
 	ld d,e
 
 puyoclear_se_sloop_low:
-	ld hl,0x500
+	ld hl,0x400
 puyoclear_se_pitchloop_low:
 	xor a
 	dec b
@@ -115,13 +115,17 @@ puyoclear_se_low_sk2:
 	jr nz,puyoclear_se_pitchloop_low
 puyoclear_se_pitchup:
 	ld a,c
-	sub 40
+	sub 50
 	ld c,a
 	ld a,e
-	sub 40
+	sub 50
 	ld e,a
+
+	ld a,10      ; threshold
+	cp e
+	jr nc, puyoclear_se_cleanup
 puyoclear_se_sloop_high:
-	ld hl,0x500
+	ld hl,0x400
 puyoclear_se_pitchloop_high:
 	xor a
 	dec b
@@ -145,98 +149,14 @@ puyoclear_se_high_sk2:
 	jr nz,puyoclear_se_pitchloop_high
 puyoclear_se_pitchdown:
 	ld a,c
-	add 50
+	add 40
 	ld c,a
-
-	ld a,120
-	cp c
-	jr c, puyoclear_se_cleanup
-
 	ld a,e
-	add 50
+	add 40
 	ld e,a
 
 	jr puyoclear_se_sloop_low
 puyoclear_se_cleanup:
-	ei
-	ret
-
-
-; ------------------------------------------------------------------
-; Plays scary sequence
-; ------------------------------------------------------------------
-scary_se:
-	di
-
-	; initial pitches
-	ld c,100
-	ld e,80
-	ld b,c
-	ld d,e
-
-scary_se_sloop_low:
-	ld hl,0x1200
-scary_se_pitchloop_low:
-	xor a
-	dec b
-	jr nz, scary_se_low_sk1
-	ld b,c
-	ld a,$10
-scary_se_low_sk1:
-	out ($fe),a
-
-	xor a
-	dec d
-	jr nz, scary_se_low_sk2
-	ld d,e
-	ld a,$10
-scary_se_low_sk2:
-	out ($fe),a
-
-	dec hl
-	ld a,l
-	or h
-	jr nz,scary_se_pitchloop_low
-scary_se_pitchup:
-	ld a,c
-	sub 40
-	ld c,a
-	ld a,e
-	sub 40
-	ld e,a
-scary_se_sloop_high:
-	ld hl,0x1000
-scary_se_pitchloop_high:
-	xor a
-	dec b
-	jr nz, scary_se_high_sk1
-	ld b,c
-	ld a,$10
-scary_se_high_sk1:
-	out ($fe),a
-
-	xor a
-	dec d
-	jr nz, scary_se_high_sk2
-	ld d,e
-	ld a,$10
-scary_se_high_sk2:
-	out ($fe),a
-
-	dec hl
-	ld a,l
-	or h
-	jr nz,scary_se_pitchloop_high
-scary_se_pitchdown:
-	ld a,c
-	add 50
-	ld c,a
-	ld a,e
-	add 50
-	ld e,a
-
-	jr scary_se_sloop_low
-scary_se_cleanup:
 	ei
 	ret
 
